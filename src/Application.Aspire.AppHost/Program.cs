7a922a4ebb390b4f70f0de3f5386290212eec57e
Davis Kolakowski <DavisKolakowski@users.noreply.github.com>
2025-06-27 17:39:21 -0400

    feat: add Azure PostgreSQL package reference, update application configuration, and enhance Docker setup

--- src/Application.Aspire.AppHost/Program.cs
+++ src/Application.Aspire.AppHost/Program.cs
@@ -1,34 +1,18 @@
+using Aspire.Hosting;
+
+using Azure.Provisioning.PostgreSql;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var db = 
-//    builder.AddPostgres("postgres")
-//        .WithDataBindMount(
-//            source: @"..\..\data\postgresql",
-//            isReadOnly: false
-//        )
-//        .WithImage("postgis/postgis")
-//        .WithPgAdmin()
-//        .WithPgWeb()
-//        .AddDatabase("application-db");
-
-//var cache = 
-//    builder.AddRedis("cache")
-//        .WithDataBindMount(
-//            source: @"..\..\data\redis",
-//            isReadOnly: false
-//        )
-//        .WithRedisInsight()
-//        .WithRedisCommander();
-
-var db =
-    builder.AddPostgres("postgres")
-        .WithVolume(
-            name: "postgres-data",
-            target: "/var/lib/postgresql/data",
-            isReadOnly: false
-        )
-        .WithImage("postgis/postgis")
-        .AddDatabase("application-db");
+var db = builder.AddPostgres("postgres")
+    .WithVolume(
+        name: "postgres-data",
+        target: "/var/lib/postgresql/data",
+        isReadOnly: false
+    )
+    .WithImage("postgis/postgis")
+    .WithPgAdmin()
+    .AddDatabase("application-db");
 
 var databaseMigrations = 
     builder.AddProject<Projects.Application_Services_DatabaseMigrations>("database-migrations")
@@ -42,8 +26,13 @@ var server =
 
 var client =
     builder.AddNpmApp("web-client", @"..\Application.Clients.Web", "dev")
-        .WithExternalHttpEndpoints()
         .WithReference(server)
-        .WaitFor(server);
+        .WaitFor(server)
+        .WithExternalHttpEndpoints()
+        .WithEnvironment("VITE_AUTH0_DOMAIN", builder.AddParameter("auth0-domain"))
+        .WithEnvironment("VITE_AUTH0_CLIENT_ID", builder.AddParameter("auth0-client-id", secret: true))
+        .WithEnvironment("VITE_AUTH0_AUDIENCE", builder.AddParameter("auth0-audience"))
+        .WithEnvironment("VITE_API_BASE_URL", server.GetEndpoint("https"))
+        .PublishAsDockerFile();
 
 builder.Build().Run();
