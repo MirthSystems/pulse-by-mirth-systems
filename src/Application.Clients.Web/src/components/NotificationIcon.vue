7cc5da9096e34f717b671cd40f976fbb8750db4d
Davis Kolakowski <DavisKolakowski@users.noreply.github.com>
2025-06-29 20:01:52 -0400

    refactor: Remove unused permission service and related JSON files

--- src/Application.Clients.Web/src/components/NotificationIcon.vue
+++ src/Application.Clients.Web/src/components/NotificationIcon.vue
@@ -10,7 +10,7 @@
       <!-- Notification Badge -->
       <span
         v-if="notificationCount > 0"
-        class="absolute -top-1 -right-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"
+        class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-600 rounded-full transform translate-x-1/2 -translate-y-1/2"
       >
         {{ notificationCount > 99 ? '99+' : notificationCount }}
       </span>
